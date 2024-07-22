4238c7f22f7672681e1bbf75153beb31ee281019
luzair <m500.abdullah@gmail.com>
2024-07-22 17:16:47 +0500

    string

--- app/main.py
+++ app/main.py
@@ -43,6 +43,22 @@ class Scanner:
         char_combo = char +next_char
         if (char_combo == '//'):
             self.eoline()
+        elif (char == '"'):
+            strVal = "\""
+            self.current+=1
+            while (self.isAtEnd()!=True and self.source[self.current] != '"'):
+                stringVal += self.source[self.current]
+                if (self.source[self.current] == '\n'): self.lineNum +=1
+                self.current+=1
+            stringVal += '\"'
+            if (self.isAtEnd()):
+                self.reportError(self.lineNum, "Unterminated string.")
+                return
+
+            newToken = Token("STRING", stringVal, stringVal[1:-1], self.lineNum)
+            self.addToken(newToken)
+            print(newToken.tokenisedForm())         
+
         elif (char_combo in tokenDict):
             newToken = Token(tokenDict[char_combo], char_combo, "null", self.lineNum)
             print(newToken.tokenisedForm())
@@ -57,19 +73,22 @@ class Scanner:
             self.current+=1
             if (char == '\n'): self.lineNum+=1
         else:
-            self.reportError(self.lineNum, char)
+            self.reportError(self.lineNum,"Unexpected character", char)
             self.current+=1
 
     def eoline(self):
-        while (self.current < len(self.source) and self.source[self.current] != '\n'):
+        while (self.isAtEnd()!=True and self.source[self.current] != '\n'):
             self.current+=1
 
     def isAtEnd(self) -> bool:
         return (self.current >= len(self.source))
 
-    def reportError(self, lineNum, char):
+    def reportError(self, lineNum, errType, char=''):
         self.hadError = True
-        print(f"[line {lineNum}] Error: Unexpected character: {char}", file=sys.stderr)
+        if errType=="Unexpected character":
+            print(f"[line {lineNum}] Error: {errType}: {char}", file=sys.stderr)
+        else:
+            print(f"[line {lineNum}] Error: {errType}", file=sys.stderr)
 
 tokenDict = {
     '(': "LEFT_PAREN",
