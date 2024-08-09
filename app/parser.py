e2f7b54ea3b167e7922eca41ce0ff779d8b28bc0
luzair <m500.abdullah@gmail.com>
2024-08-09 14:52:04 +0500

    bool parsing

--- app/parser.py
+++ app/parser.py
@@ -19,6 +19,7 @@ class Parser:
 
     def parse(self):
         currToken = self.tokenss[self.curr]
+        print(currToken.lexeme)
         while(not self.isAtEnd() and currToken!="EOF"):
             print(self.parse_token(currToken))
             self.curr +=1 if not self.isAtEnd() else 0
