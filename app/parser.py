f162f833f4ef659a7c0973d9c826314bee69689a
luzair <m500.abdullah@gmail.com>
2024-08-09 20:11:26 +0500

    factoring operators

--- app/parser.py
+++ app/parser.py
@@ -59,7 +59,7 @@ class Parser:
         expr= self.unary()
         token = self.tokenss[self.curr]
         while token.tokenType=="STAR" or token.tokenType =="SLASH":
-            print(token[i].lexeme)
+            print(token.lexeme)
             operator = token.lexeme
             self.curr+=1
             right = self.unary()
