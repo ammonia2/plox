db80e9e78f24e3ac7f6ed0cf3632b52b8644b7ad
luzair <m500.abdullah@gmail.com>
2024-08-10 15:17:25 +0500

    interpreting unary

--- app/interpreter.py
+++ app/interpreter.py
@@ -7,4 +7,8 @@ class Interpreter:
             return expr.value
         elif isinstance(expr, Grouping):
             return self.interpret(expr.expression)
-        
+        elif isinstance(expr, Unary):
+            if expr.operator == "-":
+                return -self.interpret(expr.right)
+            elif expr.operator == "!":
+                return not self.interpret(expr.right)
