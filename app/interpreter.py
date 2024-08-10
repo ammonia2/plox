724b94f16a354d1952d40480121f05b9d33513b8
luzair <m500.abdullah@gmail.com>
2024-08-10 13:03:14 +0500

    interpreting literals 2

--- app/interpreter.py
+++ app/interpreter.py
@@ -4,10 +4,9 @@ class Interpreter:
         if expr == "true" or expr=="false" or expr=="nil" or isinstance(expr, str):
             return expr
         elif isinstance(expr, str):
-            try:
+            if expr.isdigit() or (expr[0] == '-' and expr[1:].isdigit()):
                 return int(expr)
+            try:
+                return float(expr)
             except ValueError:
-                try:
-                    return float(expr)
-                except ValueError:
-                    return expr
+                return expr
