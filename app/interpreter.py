5a7b0e8ef36a6534ed4b5e93c4b2e335eb155b96
luzair <m500.abdullah@gmail.com>
2024-08-10 14:27:06 +0500

    interpreting literals 2

--- app/interpreter.py
+++ app/interpreter.py
@@ -5,9 +5,3 @@ class Interpreter:
     def interpret(self, expr):
         if isinstance(expr, Literal):
             return expr.value
-        elif int(expr):
-            return int(expr)
-        elif float(expr):
-            return float(expr)
-        else:
-            return expr
