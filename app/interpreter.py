e1fc171b368fbb7941a1f9a451ff1703ac31863a
luzair <m500.abdullah@gmail.com>
2024-08-10 13:08:10 +0500

    interpreting literals 2

--- app/interpreter.py
+++ app/interpreter.py
@@ -4,15 +4,7 @@ class Interpreter:
         if expr == "true" or expr=="false" or expr=="nil" or isinstance(expr, str):
             return expr
         elif isinstance(expr, str):
-            try:
-                retVal = float(expr)
-                if retVal.is_integer():
-                    retVal = int(retVal)
-                return retVal
-            except ValueError:
-                return expr
-        elif isinstance(expr, (int, float)):
-            retVal = expr
-            if isinstance(retVal, float) and retVal.is_integer():
-                retVal = int(retVal)
-            return retVal
+            if '.' in expr:
+                return float(expr)
+            else:
+                return int(expr)
