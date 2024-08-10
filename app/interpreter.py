7ed0d468fb2c61140a1647a619db9e7ae9d1eaf0
luzair <m500.abdullah@gmail.com>
2024-08-10 13:16:21 +0500

    interpreting literals 2

--- app/interpreter.py
+++ app/interpreter.py
@@ -3,8 +3,7 @@ class Interpreter:
     def interpret(self, expr):
         if expr == "true" or expr=="false" or expr=="nil" or isinstance(expr, str):
             return expr
-        elif isinstance(expr, str):
-            if '.' in expr:
-                return float(expr)
-            else:
-                return int(expr)
+        elif expr.isdigit():
+            return int(expr)
+        elif a.replace('.','',1).isdigit() and a.count('.') < 2:
+            return float(expr)
