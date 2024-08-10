5156219609e5f66f9297bf10dc9b9c9e15926b17
luzair <m500.abdullah@gmail.com>
2024-08-10 15:11:51 +0500

    interpreting literals 2

--- app/interpreter.py
+++ app/interpreter.py
@@ -5,3 +5,6 @@ class Interpreter:
     def interpret(self, expr):
         if isinstance(expr, Literal):
             return expr.value
+        elif isinstance(expr, Grouping):
+            return self.interpret(expr.expression)
+        
